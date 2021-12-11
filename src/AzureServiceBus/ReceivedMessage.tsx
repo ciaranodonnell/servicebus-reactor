f1f162ae2aa9416fd1e783876f1dd0e18fc36347
Ciaran O'Donnell <ciaran.odonnell@avanade.com>
2021-12-10 21:34:30 -0500

    added the ability to peak & cleaned up the tabs component

---
+++ src/AzureServiceBus/ReceivedMessage.tsx
@@ -0,0 +1,33 @@
+import * as asb from "@azure/service-bus";
+import { Queue } from "./Queue";
+import { Topic } from "./Topic";
+
+export class ReceivedMessage {
+    private receiveMode: string;
+    private receviedFrom: Queue | Topic;
+    body: string | undefined;
+    deliveryCount: number | undefined;
+    enqueuedTimeUtc: Date | undefined;
+    scheduledEnqueueTimeUtc: Date | undefined;
+    messageId: string | number | Buffer | undefined;
+    sequenceNumber: Long | undefined;
+    size: number | undefined;
+    subject: string | undefined;
+    timeToLive: number | undefined;
+    to: string | undefined;
+
+    constructor(recieveMode: string, receivedFrom: Queue | Topic, message: asb.ServiceBusReceivedMessage | undefined) {
+        this.receiveMode = recieveMode;
+        this.receviedFrom = receivedFrom;
+        this.body = message?.body;
+        this.deliveryCount = message?.deliveryCount;
+        this.enqueuedTimeUtc = message?.enqueuedTimeUtc;
+        this.scheduledEnqueueTimeUtc = message?.scheduledEnqueueTimeUtc;
+        this.messageId = message?.messageId;
+        this.sequenceNumber = message?.sequenceNumber;
+        this.size = message?.body.length;
+        this.subject = message?.subject;
+        this.timeToLive = message?.timeToLive;
+        this.to = message?.to;
+    }
+}
---
+++ src/TabPanel.tsx
@@ -0,0 +1,88 @@
+import React, { Children, ReactNode } from "react";
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import ReactDOM from "react-dom";
+
+
+type TabControlProps = React.PropsWithChildren<{ tabGroupName: string, onChange?: (newValue: number) => void }>;
+
+type TabProps = React.PropsWithChildren<{ title: string }>;
+
+class TabPanel extends React.Component<TabProps> {
+    get title() { return this.props.title; }
+    get children() { return this.props.children; }
+    render() { return (<div>{this.props.children}</div>); }
+}
+
+function TabControl(props: TabControlProps): JSX.Element {
+    let tabs: TabPanel[] = [];
+    const tabGroupName = props.tabGroupName;
+
+    React.Children.map(props.children, (child: React.ReactNode) => {
+
+        if ((child as any).type.name == "TabPanel") {
+            console.log("mapped a tab child");
+            tabs.push(child as TabPanel);
+        } else {
+            console.log("skipped a child", child instanceof TabPanel);
+            console.log(child)
+        }
+    });
+
+    const { onChange, ...other } = props;
+    const [selectedTab, setSelectedTab] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setSelectedTab(newValue);
+    };
+    function a11yProps(index: number) {
+        return {
+            id: `simple-tab-${tabGroupName}-${index}`,
+            'aria-controls': `simple-tabpanel-${tabGroupName}-${index}`,
+        };
+    }
+
+    function writeTabsPanels(child: TabPanel, index: number) {
+        const isSelected = selectedTab == index;
+        const divId = `simple-tabpanel-${index}`;
+        console.log("writing tab panel", index, child, "isSelected", isSelected);
+        return (
+            <div
+                role="tabpanel"
+                hidden={!isSelected}
+                id={divId}
+                key={divId}
+                aria-labelledby={`simple-tab-${tabGroupName}-${index}`}
+            >
+                {isSelected ? (
+                    <Box sx={{ p: 3 }}>
+                        <Typography>
+                            {child.props.children}
+                        </Typography>
+                    </Box>
+                ) : (<></>)}
+            </div>
+        );
+    }
+
+    console.log("Tab Count", tabs.length);
+    console.log("Selected Tab", selectedTab);
+    return (
+
+        <Box className="thisIsTheBox" {...other}>
+            <Tabs value={selectedTab} onChange={handleChange} variant="fullWidth" >
+                {tabs.map((child, index) => {
+                    console.log("Tab", child.props.title, index);
+                    return (<Tab key={index} {...a11yProps(index)} label={child.props.title} />);
+                })}
+            </Tabs>
+            {tabs.map((child, index) => {
+                return writeTabsPanels(child, index);
+            })}
+        </Box>
+    );
+}
+
+export { TabPanel, TabControl };
